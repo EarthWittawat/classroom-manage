9260ec853bb6f21bbfce091b44e7a11b22767bdb
earthwittawat2548 <63118195+earthwittawat2548@users.noreply.github.com>
2022-05-11 12:55:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,10 @@ sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 st.header("SMTE 6/11 Homework List")
-with st.sidebar:
-    Name = st.selecbox(
-        "ชื่อ-นามสกุล",
-        ("", "")
-    )
+Name = st.sidebar.selectbox(
+    "ชื่อ-นามสกุล",
+    ("Email", "Home phone", "Mobile phone")
+)
 # Print results.
 for row in rows:
     st.write(f"{row.name} has a :{row.pet}:")
